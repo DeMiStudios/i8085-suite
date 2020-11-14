ac1e3f174bedcbd828df62877c2e944ec70e375d
Matthew <willyoumeryme@gmail.com>
2020-11-13 16:05:43 -0800

    assembler: Clean up code

--- src/Assembler/utility/Integer.ts
+++ src/Assembler/utility/Integer.ts
@@ -3,14 +3,14 @@ import { assert } from "Shared/Debug";
 export function isValidInteger(value: string): boolean {
 	return (
 		/^[0-9][0-9_]*$/.test(value) ||
-		/^[0-1][0-1_]*[bB]$/.test(value) ||
-		/^[0-7][0-7_]*[oO]$/.test(value) ||
-		/^[0-9][0-9_]*[dD]$/.test(value) ||
-		/^[0-9][0-9A-Fa-f_]*[hH]$/.test(value) ||
 		/^0[bB][01_]+$/.test(value) ||
 		/^0[oO][0-7_]+$/.test(value) ||
 		/^0[dD][0-9_]+$/.test(value) ||
-		/^0[xX][0-9A-Fa-f_]+$/.test(value)
+		/^0[xX][0-9A-Fa-f_]+$/.test(value) ||
+		/^[0-1][0-1_]*[bB]$/.test(value) ||
+		/^[0-7][0-7_]*[oO]$/.test(value) ||
+		/^[0-9][0-9_]*[dD]$/.test(value) ||
+		/^[0-9][0-9A-Fa-f_]*[hH]$/.test(value)
 	);
 }
 
@@ -31,10 +31,10 @@ export function parseInteger(value: string): number | undefined {
 			}
 		}
 
-		if (suffix) {
-			result = parseInt(value.substr(0, value.length - 1).replace(/_/g, ""), getRadixFromModifier(suffix));
-		} else if (prefix && !suffix) {
+		if (prefix) {
 			result = parseInt(value.substr(2).replace(/_/g, ""), getRadixFromModifier(prefix));
+		} else if (suffix) {
+			result = parseInt(value.substr(0, value.length - 1).replace(/_/g, ""), getRadixFromModifier(suffix));
 		} else {
 			result = parseInt(value.replace(/_/g, ""), 10);
 		}
@@ -46,23 +46,15 @@ export function parseInteger(value: string): number | undefined {
 	}
 }
 
-function getRadixFromModifier(modifier: string): number | undefined {
-	switch (modifier) {
+function getRadixFromModifier(modifier: string): number {
+	switch (modifier.toLowerCase()) {
 		case "b":
-		case "B":
 			return 2;
 		case "o":
-		case "O":
 			return 8;
 		case "d":
-		case "D":
 			return 10;
-		case "h":
-		case "H":
-		case "x":
-		case "X":
-			return 16;
 		default:
-			return undefined;
+			return 16;
 	}
 }
